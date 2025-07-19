f3da2b36ee369464bfafd96fe81ee35a30b4f5bf
Wrenchded <98300872+Wrenchded@users.noreply.github.com>
2025-07-20 02:11:24 +0530

    Update language_utils.py

--- language_utils.py
+++ language_utils.py
@@ -1,16 +1,15 @@
+from deep_translator import GoogleTranslator
 from langdetect import detect
-from googletrans import Translator
-
-translator = Translator()
 
 def detect_language(text):
     try:
         return detect(text)
-    except:
-        return "en"
-    
-def translate_to_english(text, src_lang):
-    if src_lang == "en":
-        return text 
-    translation = translator.translate(text, src=src_lang, dest='en')
-    return translation.text
+    except Exception as e:
+        return "unknown"
+
+def translate_to_english(text):
+    try:
+        translated = GoogleTranslator(source='auto', target='en').translate(text)
+        return translated
+    except Exception as e:
+        return text
